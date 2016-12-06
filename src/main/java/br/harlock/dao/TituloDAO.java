77311ba9f7d791af24cf9790ffd7e88c648fcf64
João Paulo S. Almeida <789joaopaulo@gmail.com>
2016-12-06 12:43:48 -0200

    Listagem de titulos na home

--- src/main/java/br/harlock/dao/TituloDAO.java
+++ src/main/java/br/harlock/dao/TituloDAO.java
@@ -136,8 +136,46 @@ public class TituloDAO {
         }
 
     }
+    
+    public Iterator<Titulo> consutlarporTitulo(String termo) throws Exception{
+        try {
+            ArrayList lista = new ArrayList();
+            String sql = "SELECT * from titulo where obra REGEXP '?'";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, termo);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                boolean next = rs.next();
+                Titulo titulo = new Titulo();
+                titulo.setIdTitu(rs.getInt("ID_TITU"));
+                titulo.setIsbn(rs.getString("ISBN"));
+                titulo.setIssn(rs.getString("ISSN"));
+                titulo.setObra(rs.getString("obra"));
+                titulo.setDescricao(rs.getString("Descricao"));
+                titulo.setDataDePublicacao(rs.getString("DataDePublicacao"));
+                titulo.setCidadePublicacao(rs.getString("CidadePublicacao"));
+                titulo.setEstadoPublicacao(rs.getString("EstadoPublicacao"));
+                titulo.setEdicao(rs.getString("Edicao"));
+                titulo.setIdioma(rs.getString("Idioma"));
+                titulo.setTraducao(rs.getString("Traducao"));
+                //titulo.setCapa(rs.getBlob("Capa"));
+                titulo.setFkItemPdc(rs.getInt("FK_ITEM_PDC"));
+                titulo.setCategoriaitemacervo(new CategoriaDAO().Pesquisar(rs.getInt("FK_CAT_ARCE")));
+                titulo.setFkItemAcervo(rs.getInt("FK_CAT_ARCE"));
+                titulo.setTipoDeObra(rs.getString("tipoDeObra"));
+                titulo.setDuracao(rs.getFloat("duracao"));
+                titulo.setVolume(rs.getString("volume"));
+                titulo.setQuantidadePaginas(rs.getInt("quatidadepaginas"));
+                lista.add(titulo);
+            }
+            return lista.iterator();
+            
+        } catch (Exception e) {
+             throw new Exception(" r"+e);
 
-    public Iterator<Titulo> ConsultarTodos() throws ParseException, Exception {
+        }
+    }
+      public Iterator<Titulo> ConsultarTodos() throws ParseException, Exception {
         
         try {
             List<Titulo> titulos = new ArrayList<Titulo>();
--- src/main/webapp/template/paginas/home.jsp
+++ src/main/webapp/template/paginas/home.jsp
@@ -1,3 +1,4 @@
+
 <%@page import="br.harlock.model.Titulo"%>
 <%@page import="java.util.Iterator"%>
 <%@page import="br.harlock.dao.TituloDAO"%>
@@ -7,16 +8,28 @@
     <a href="#"><img class="search-icon" src="template/imgs/search-icon.png"></a>
   </form>
 </div>
+  <div class="containerMD">
+      <form class="search-container" method="post" action="index.jsp">
+      <input type="text" id="search-bar" name="pesquisa" placeholder="O que posso te ajudar a encontrar hoje?">
+      <button><img class="search-icon" src="imgs/search-icon.png"></button>
+    </form>
+  </div>
 <div class="espaceembranco">
 
 </div>
 <%
     TituloDAO dao = new TituloDAO();
-    Iterator listar = (Iterator) dao.ConsultarTodos();
+    Iterator listar = null;
+    
+    if(request.getParameter("pesquisa") != null){ 
+       listar =  dao.consutlarporTitulo(request.getParameter("pesquisa"));
+    }else{
+       listar =  dao.ConsultarTodos(); 
+    }
     int cont = 1;
     %>
 
-
+<%if(listar.hasNext()){%>
 <ul class="exibeListaTitulo">
     <% while(listar.hasNext() && cont !=5){
         cont++;
@@ -29,4 +42,6 @@
     <%}%>
   
 </ul>
-
+<%}else{%>
+<h2><center>A pesquisa obteve zero resultado</center>
+<%}%>
